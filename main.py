53e3f4c087243868d090f0448fa49d2cfc9c8c19
Pedro Del Valle <pedrodebdv@gmail.com>
2025-03-24 11:25:48 -0300

    Changing main.py

--- main.py
+++ main.py
@@ -3,28 +3,38 @@ import os
 import uuid
 import traceback
 from datetime import datetime
-
+import threading
 
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
-from datetime import datetime, timedelta
 
 
 app = FastAPI()
 
-
 class SVGInput(BaseModel):
     svg_code: str
 
+def schedule_file_deletion(file_path: str, delay: int = 3600):
+    def delete_file():
+        try:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                print (f"Arquivo {file_path} removido após 1 hora.")
+        except Exception as e: 
+            print(f"Erro ao remover arquivo {file_path}: {str(e)}")
+
+    timer = threading.Timer(delay, delete_file)
+    timer.start()
+
 @app.post("/generate-video/")
-async def generate_video(svg_data: dict):
+async def generate_video(svg_input: SVGInput):
     try:
-        svg_code = svg_data.get("svg")
+        svg_code = svg_input.svg_code
         if not svg_code:
             raise ValueError("Campo 'svg' vazio ou ausente")
         
-        filename= f"video_{datetime.now().strftime('%Y%m%d%H%M%S')}.mp4"
+        filename= f"video_{uuid.uuid4().hex}.mp4"
         output_path = os.path.abspath(os.path.join("generated_videos", filename))
         os.makedirs("generated_videos", exist_ok=True)
 
@@ -37,6 +47,8 @@ async def generate_video(svg_data: dict):
             raise RuntimeError(f"Arquivo não foi gerado: {output_path}")
         else:
             print("Arquivo gerado com sucesso e presente em disco:", output_path)
+
+        schedule_file_deletion(output_path, delay=3600)
 
         return {
             "message": "Vídeo criado com sucesso",
